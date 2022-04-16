3f92d3ee94a022ed4eb021b04429449122c961c6
Azizbek <kobilov.2000@mail.ru>
2022-04-16 12:19:21 +0500

    Testing

---
+++ src/com/kobilov/homework08/game/GameWinnerPrinter.java
@@ -0,0 +1,5 @@
+package com.kobilov.homework08.game;
+
+public interface GameWinnerPrinter {
+    void printWinner(Player winner);
+}
---
+++ src/com/kobilov/homework08/game/Player.java
@@ -0,0 +1,13 @@
+package com.kobilov.homework08.game;
+
+public class Player {
+    private final String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
